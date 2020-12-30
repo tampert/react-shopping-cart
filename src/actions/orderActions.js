dba6a6c08468cf3d2b0b78c18ff47e6c40e170ba
tampert <d_soentjens@hotmail.com>
2020-12-30 12:48:38 +0100

    create order

---
+++ src/actions/orderActions.js
@@ -0,0 +1,21 @@
+import { CLEAR_ORDER, CREATE_ORDER,CLEAR_CART } from "../types"
+
+export const createOrder = (order) => (dispatch) => {
+    fetch("/api/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: CREATE_ORDER, payload: data });
+        localStorage.clear("cartItems");
+        dispatch({ type: CLEAR_CART });
+      });
+  };
+
+export const clearOrder = () => (dispatch) => {
+    dispatch({ type:CLEAR_ORDER})
+}
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,5 +1,5 @@
 
-import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
+import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from "../types";
 
 export const cartReducer = (
   state = { cartItems: JSON.parse(localStorage.getItem("cartItems") || "[]")  },
@@ -10,6 +10,8 @@ export const cartReducer = (
       return { cartItems: action.payload.cartItems };
     case REMOVE_FROM_CART:
       return { cartItems: action.payload.cartItems };
+    case CLEAR_CART:
+        return { cartItems: [] };
     default:
       return state;
   }

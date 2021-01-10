c88110f07f6cbf9e163606b1257a94eb3af87541
tampert <d_soentjens@hotmail.com>
2021-01-10 01:01:52 +0100

    add-env

--- server.js
+++ server.js
@@ -1,84 +1,105 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const shortid = require('shortid');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/react-shopping-cart-db",{
-    useNewUrlParser:true,
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology:true
-});
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`connected to mogodb.${process.env.MONGODB_URL}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-const Product = mongoose.model('products', new mongoose.Schema({
-    _id: {type:String, default:shortid.generate },
+const Product = mongoose.model(
+  "products",
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
     title: String,
     description: String,
     image: String,
     price: Number,
-    availableSizes: [String]
-}));
+    availableSizes: [String],
+  })
+);
 
-app.get("/api/products", async (req,res) => {
-    // Check if there are any products. Find is a promise
-    const products = await Product.find({});
-    res.send(products);
+app.get("/api/products", async (req, res) => {
+  // Check if there are any products. Find is a promise
+  const products = await Product.find({});
+  res.send(products);
 });
 
-app.post("/api/products", async (req,res) => {
-    const newProduct = new Product(req.body);
-    const savedProduct = await newProduct.save();
-    res.send(savedProduct);
-})
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
 
-app.delete("/api/products/:id", async(req,res)=>{
-    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-    res.send(deletedProduct);
-})
+app.delete("/api/products/:id", async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
 
-const Order = mongoose.model("order", new mongoose.Schema({
-    _id: {
-        type:String,
-        default:shortid.generate
-    },
-    email:String,
-    name:String,
-    adress:String,
-    total:Number,
-    cartItems : [
-        {
-        _id:String,
-        title:String,
-        price:Number,
-        count:Number
-        }
-    ]},
+const Order = mongoose.model(
+  "order",
+  new mongoose.Schema(
     {
-    timestamps:true
-}));
-
-app.post("/api/orders", async(req,res) => {
-    if(!req.body.name ||
-        !req.body.email ||
-        !req.body.adress ||
-        !req.body.total ||
-        !req.body.cartItems 
-    ){
-        return res.send({message:"data is required."})
+      _id: {
+        type: String,
+        default: shortid.generate,
+      },
+      email: String,
+      name: String,
+      adress: String,
+      total: Number,
+      cartItems: [
+        {
+          _id: String,
+          title: String,
+          price: Number,
+          count: Number,
+        },
+      ],
+    },
+    {
+      timestamps: true,
     }
-    const order = await Order(req.body).save();
-    res.send(order)
-})
+  )
+);
 
-app.get("/api/orders", async (req,res) => {
-    // Check if there are any products. Find is a promise
-    const orders = await Order.find({});
-    res.send(orders);
+app.post("/api/orders", async (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.adress ||
+    !req.body.total ||
+    !req.body.cartItems
+  ) {
+    return res.send({ message: "data is required." });
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
+});
+
+app.get("/api/orders", async (req, res) => {
+  // Check if there are any products. Find is a promise
+  const orders = await Order.find({});
+  res.send(orders);
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log("server at http://localhost:5000")
-})
+  console.log("server at http://localhost:5000");
+});
